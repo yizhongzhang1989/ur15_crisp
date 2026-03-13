f7b7c598dc789faa98a4c9a6e2b4aa459f275527
yizhongzhang1989 <yizhongzhang1989@gmail.com>
2026-03-13 18:23:39 +0800

    UR15 able to move (#1)
    * Add CRISP controllers (submodule) and UR15 bringup package
    
    - Add crisp_controllers v2.1.0 as git submodule
    - Create crisp_ur15_bringup package with:
      - ur15_controllers.yaml: controller_manager config with CRISP controllers
      - ur15_crisp.launch.py: launch file wrapping ur_control.launch.py
    - Add detailed testing plan (plan.md)
    - Update README with setup instructions for new machines
    
    * Update plan.md: crisp_controllers is now a submodule, add OOM build warning
    
    * Improve new-machine setup: add prereqs, shallow submodule, fix package deps
    
    - .gitmodules: add shallow=true for faster submodule clone
    - README: add Prerequisites section (ROS, UR driver, RAM, network, URCap)
    - README: use --shallow-submodules in clone command
    - package.xml: add crisp_controllers dependency for correct build order
    - plan.md: update clone instructions to match
    
    * zero gravity working
    
    * solved the damping issue
    
    * updated readme

---
+++ src/crisp_ur15_bringup/setup.py
@@ -0,0 +1,36 @@
+import os
+from glob import glob
+
+from setuptools import find_packages, setup
+
+package_name = 'crisp_ur15_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+            glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'),
+            glob(os.path.join('config', '*.yaml'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='a',
+    maintainer_email='yizhongzhang1989@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
