77ba745bcf34a049a6c8fb4f7c6658b6296e6758
yizhongzhang <yizhongzhang1989@gmail.com>
2026-03-24 19:28:17 +0800

    initialized teleop with alicia

---
+++ src/alicia_teleop/setup.py
@@ -0,0 +1,24 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = "alicia_teleop"
+
+setup(
+    name=package_name,
+    version="0.1.0",
+    packages=[package_name],
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
+        (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    entry_points={
+        "console_scripts": [
+            "teleop_node = alicia_teleop.teleop_node:main",
+        ],
+    },
+)
