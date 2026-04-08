43e8e28098e13c0872d93ca3c6662ddd1731e05d
yizhongzhang <yizhongzhang1989@gmail.com>
2026-04-08 15:26:12 +0800

    fixed an error of web

--- src/data_collection/scripts/dashboard_node.py
+++ src/data_collection/scripts/dashboard_node.py
@@ -159,13 +159,16 @@ class DashboardHandler(http.server.SimpleHTTPRequestHandler):
             self.send_error(404)
 
     def _json_response(self, data):
-        body = json.dumps(data).encode()
-        self.send_response(200)
-        self.send_header("Content-Type", "application/json")
-        self.send_header("Access-Control-Allow-Origin", "*")
-        self.send_header("Content-Length", str(len(body)))
-        self.end_headers()
-        self.wfile.write(body)
+        try:
+            body = json.dumps(data).encode()
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.send_header("Access-Control-Allow-Origin", "*")
+            self.send_header("Content-Length", str(len(body)))
+            self.end_headers()
+            self.wfile.write(body)
+        except (BrokenPipeError, ConnectionResetError):
+            pass
 
     def log_message(self, format, *args):
         if len(args) >= 2 and "404" in str(args[1]):
